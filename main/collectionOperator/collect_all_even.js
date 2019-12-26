1f607343c5bc8ab61d0d848a1bb2d20dc8060ecc
JinHui <jh731448520@163.com>
2019-12-27 00:24:44 +0800

    [JinHui]{feat}-{#collectionOperator}:finish 1.collect_all_even and pass test

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  var result = [];
+  var length = collection.length;
+  for (var i = 0; i < length; i++) {
+    if (collection[i] % 2 == 0) {
+      result.push(collection[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = collect_all_even;
