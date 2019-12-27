94343588eb25b16104c00e749575222880f48cd3
JinHui <jh731448520@163.com>
2019-12-27 15:55:31 +0800

    [JinHui]{feat}-{#collectionOperator}:add new filter solution to 1.collect_all_even and pass test

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -1,14 +1,15 @@
 'use strict';
 
 function collect_all_even(collection) {
-  var result = [];
-  var length = collection.length;
-  for (var i = 0; i < length; i++) {
-    if (collection[i] % 2 == 0) {
-      result.push(collection[i]);
-    }
-  }
+  // var result = [];
+  // var length = collection.length;
+  // for (var i = 0; i < length; i++) {
+  //   if (collection[i] % 2 == 0) {
+  //     result.push(collection[i]);
+  //   }
+  // }
+  // return result;
+  var result = collection.filter(collection => collection%2 == 0);
   return result;
 }
-
 module.exports = collect_all_even;
