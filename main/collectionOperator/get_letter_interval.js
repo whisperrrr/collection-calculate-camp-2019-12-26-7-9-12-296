26f8a79f4f5b5b959332661a87489543ee454ca4
JinHui <jh731448520@163.com>
2019-12-27 19:10:05 +0800

    [JinHui]{feat}-{#collectionOperator}:finish 7.get_letter_intervel.js and pass test

--- main/collectionOperator/get_letter_interval.js
+++ main/collectionOperator/get_letter_interval.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  var str=['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var result = [];
+    if (number_a < number_b) {
+      for (var i = number_a; i <= number_b; i++) {
+        result.push(str[i]);
+      }
+    }else if (number_a > number_b) {
+      for (var i = number_b; i <= number_a; i++) {
+        result.push(str[i]);
+      } 
+      result.reverse();
+    }else {
+      result.push(str[number_a]);
+    }
+    return result;
 }
 
 module.exports = get_letter_interval;
