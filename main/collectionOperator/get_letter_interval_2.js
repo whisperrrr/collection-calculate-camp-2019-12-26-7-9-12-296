ec97a5de7567d2ba156108aaee1e1a2bb2294757
JinHui <jh731448520@163.com>
2019-12-27 19:03:51 +0800

    [JinHui]{feat}-{#collectionOperator}:finish 6.get_letter_intervel_2.js and pass test

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -1,8 +1,31 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+  var str=[
+  '0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+  'aa','ab','ac','ad','ae','af','ag', 'ah', 'ai', 'aj', 'ak', 'al', 'am', 'an', 'ao', 'ap','aq', 'ar', 'as', 'at', 'au', 'av', 'aw', 'ax', 'ay','az', 
+  'ba'
+  ];
+  var result = [];
+  if (number_a < number_b) {
+    for (var i = number_a; i <= number_b; i++) {
+      result.push(str[i]);
+    }
+  }else if (number_a > number_b) {
+    for (var i = number_b; i <= number_a; i++) {
+      result.push(str[i]);
+    } 
+    result.reverse();
+  }else {
+    result.push(str[number_a]);
+  }
+  return result;
 }
 
 module.exports = get_letter_interval_2;
+// 字符转数字 
+// "".charCodeAt();
+// "a".charCodeAt()=97
+// 数字转字符
+// String.fromCharCode(数字);
 
