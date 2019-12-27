783d0a2f80deb2adb4e0b86cd38949255232fe8d
JinHui <jh731448520@163.com>
2019-12-27 17:25:33 +0800

    [JinHui]{feat}-{#collectionOperator}:finish 4.get_integer_interval.js and pass test

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+  var result = [];
+  var flag = number_a - number_b;
+  var max = Math.max(number_a, number_b);
+  var min = Math.min(number_a, number_b);
+  for (var i = min; i <= max; i++) {
+    result.push(i);
+  }
+  return flag<0 ? result:result.reverse();
 }
 
 module.exports = get_integer_interval;
