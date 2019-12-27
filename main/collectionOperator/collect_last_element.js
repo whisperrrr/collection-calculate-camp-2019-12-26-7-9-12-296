dd889f9795162f0f6107cccf18bb00449a2c410c
JinHui <jh731448520@163.com>
2019-12-27 20:53:58 +0800

    [JinHui]{feat}-{#collectionOperator}:add arr[index] solution to 2.collect_last_element

--- main/collectionOperator/collect_last_element.js
+++ main/collectionOperator/collect_last_element.js
@@ -1,8 +1,12 @@
 'use strict';
-
+//方法一：pop弹出最后一个元素,1ms
 function collect_last_element(collection) {
-  //在这里写入代码
   var result = collection.pop();
   return result;
 }
+//方法二：数组查找,1ms
+// function collect_last_element(collection) {
+//   var result = collection[collection.length - 1];
+//   return result;
+// }
 module.exports = collect_last_element;
