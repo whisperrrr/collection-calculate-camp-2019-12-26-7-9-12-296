935ab92518f34f770fe4eb4f26280e70f5eaf265
JinHui <jh731448520@163.com>
2019-12-27 20:05:27 +0800

    [JinHui]{feat}-{#collectionOperator}:finish 8.get_union.js and pass test

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -1,7 +1,12 @@
 'use strict';
-
+// 先求出b中a没有的元素，然后再将两个数组结果加起来
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  var result = collection_b.filter(inArray);
+  function inArray(element) {
+    return collection_a.indexOf(element) == -1;
+  }
+  result = collection_a.concat(result);
+  return result;
 }
 
 module.exports = get_union;
