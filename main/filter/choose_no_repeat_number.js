f183fb658f0b71b9cb354843db00f445137a98bb
JinHui <jh731448520@163.com>
2019-12-30 01:19:33 +0800

    [JinHui]{feat}-{#filter}:finish 3.choose_no_repeat_number and pass test

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,8 +1,11 @@
 'use strict';
-
+//思路：indexOf返回的是数组中第一个找到的元素.
+//所以第一次出现的元素返回的下标和处理中的下标是一致的
 function choose_no_repeat_number(collection) {
-
-  //在这里写入代码
+  var result = collection.filter(findNoRepeat);
+  function findNoRepeat(element,index) {
+    return collection.indexOf(element) === index;
+  }
+  return result;
 }
-
 module.exports = choose_no_repeat_number;
