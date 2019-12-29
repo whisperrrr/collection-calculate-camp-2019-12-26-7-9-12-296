c16ddbc8ed9855a096c3c2810d667bbc73e29292
JinHui <jh731448520@163.com>
2019-12-30 01:03:08 +0800

    [JinHui]{feat}-{#filter}:finish 2.choose_multiple_of_three and pass test

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
-  //在这里写入代码
+  var result = collection.filter(element => element % 3 === 0);
+  return result;
 }
 
 module.exports = choose_multiples_of_three;
