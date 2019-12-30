ff666eeb426efbd6057904ba6c2dfc07b13bd4ae
JinHui <jh731448520@163.com>
2019-12-30 21:30:55 +0800

    [JinHui]{feat}-{#filter}:finish 7.two_collections_practice_3 and pass test

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+  var result = collection_a.filter(isDivision);
+  function isDivision(element) {
+    var bLength = collection_b.length;
+    for (var i = 0; i < bLength; i++) {
+      if(element % collection_b[i] === 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+  return result;
 }
 
 module.exports = choose_divisible_integer;
