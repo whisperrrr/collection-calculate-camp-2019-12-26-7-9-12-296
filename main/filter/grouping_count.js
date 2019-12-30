47120f241dc1e3b98c6faa00824663aac0791076
JinHui <jh731448520@163.com>
2019-12-30 19:53:42 +0800

    [JinHui]{feat}-{#filter}:finish 4.grouping_count and pass test

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //在这里写入代码
+  var result = {};
+  var arrFilter = collection.filter(calculate);
+  function calculate(ele,index) {
+    if (collection.indexOf(ele) === index) {
+      result[ele] = 1;
+    }
+    else {
+      result[ele] += 1;
+    }
+    return true;
+  }
+  return result;
 }
 
 module.exports = grouping_count;
