e0929804a79fe01716229e9e713c862f57ad3e37
JinHui <jh731448520@163.com>
2019-12-31 14:40:23 +0800

    [JinHui]{feat}-{#map}:finish 3.map_to_three_multiples and pass test

--- main/map/map_to_three_multiples.js
+++ main/map/map_to_three_multiples.js
@@ -1,6 +1,7 @@
 'use strict';
 var map_to_three_multiples = function(collections){
-  return [3,9,15,12,27];
+  var result = collections.map(x => 3 * x);
+  return result;
 };
 
 module.exports = map_to_three_multiples;
