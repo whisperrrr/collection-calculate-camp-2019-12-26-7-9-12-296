cd5fc4f2cf4b58d3890f726b4fb284afa5f07273
JinHui <jh731448520@163.com>
2019-12-31 16:32:00 +0800

    [JinHui]{feat}-{#reduce}:finish 3.compare_collections and pass test

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -1,7 +1,8 @@
 'use strict';
-
+//思路：acc是累加值，保存两个数组的差。然后让cur与b进行相减，把差给acc，最后的acc为0就全部相等
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+  var result = collection_a.reduce((acc,cur,index) => acc + cur - collection_b[index]);
+  return result? true:false;
 }
 
 module.exports = compare_collections;
