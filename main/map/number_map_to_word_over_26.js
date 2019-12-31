6d752269fa590e913f28fdbe4e38e9e367bf1bdf
JinHui <jh731448520@163.com>
2019-12-31 15:17:52 +0800

    [JinHui]{feat}-{#map}:finish 4.number_map_to_word_over_26 and pass test

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,12 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  var str=[
+    '0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+    'aa','ab','ac','ad','ae','af','ag', 'ah', 'ai', 'aj', 'ak', 'al', 'am', 'an', 'ao', 'ap','aq', 'ar', 'as', 'at', 'au', 'av', 'aw', 'ax', 'ay','az', 
+    'ba'
+    ];
+  var result = collection.map(x => str[x]);
+  return result;
 };
 
 module.exports = number_map_to_word_over_26;
