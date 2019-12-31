9f174b265b08c01390bc26b5df3933b79437be80
JinHui <jh731448520@163.com>
2019-12-31 15:19:56 +0800

    [JinHui]{feat}-{#map}:finish 5.number_map_to_word and pass test

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,8 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  var str=['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var result = collection.map(x => str[x]);
+  return result;
 };
 
 module.exports = number_map_to_word;
