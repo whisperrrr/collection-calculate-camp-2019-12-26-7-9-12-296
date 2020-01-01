c3282384e2fb3c243b17beca3b0a71cb2c63475c
JinHui <jh731448520@163.com>
2020-01-01 22:16:50 +0800

    [JinHui]{feat}-{#reduce}:finish 7.compute_sum and pass test

--- main/reduce/compute_sum.js
+++ main/reduce/compute_sum.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function calculate_elements_sum(collection) {
-  //在这里写入代码
+  var result = collection.reduce((acc,cur) => acc + cur);
+  return result;
 }
 
 module.exports = calculate_elements_sum;
