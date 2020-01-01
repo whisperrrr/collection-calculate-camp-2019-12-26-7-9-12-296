146398cd2e6d9aecbec8a5527e6806a7ec176113
JinHui <jh731448520@163.com>
2020-01-01 23:45:50 +0800

    [JinHui]{feat}-{#section_2}:finish 1.practice_1 and pass test

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,21 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  //对于排好序的情况，考虑左右指针
+  var left = 0;
+  var right = left + 1;
+  var result = [];
+  var resultCount;
+  var arrLength = collection.length;
+  while (right <= arrLength) {
+    if (collection[left] !== collection[right]) {
+      resultCount = {}
+      resultCount.key = collection[left];
+      resultCount.count = right - left;
+      left = right;
+      result.push(resultCount);
+    }
+    right += 1;   
+  }
+  return result;
 }
 
 module.exports = count_same_elements;
