dc3145990675641221fb14eec197a35fffce5f1b
JinHui <jh731448520@163.com>
2020-01-01 23:16:14 +0800

    [JinHui]{feat}-{#section_1}:finish 3.practice_3 and pass test

--- main/section_1/practice_3/practice.js
+++ main/section_1/practice_3/practice.js
@@ -1,5 +1,8 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  var result;
+  collection_b = object_b.value;
+  result = collection_a.filter(ele => collection_b.indexOf(ele) > -1);
+  return result;
 }
 
 module.exports = collect_same_elements;
