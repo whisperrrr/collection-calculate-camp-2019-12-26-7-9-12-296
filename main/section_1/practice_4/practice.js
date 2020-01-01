691d21c826b31ed4e919c24cb67514dbfd19a8f4
JinHui <jh731448520@163.com>
2020-01-01 23:27:40 +0800

    [JinHui]{feat}-{#section_1}:finish 4.practice_4 and pass test

--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,5 +1,16 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  var arrA = getArrKey(collection_a);
+  var arrB = object_b.value;
+  var result = arrA.filter(ele => arrB.indexOf(ele) > -1);
+  function getArrKey(collection_a) {
+    var arrA = [];
+    var aLength = collection_a.length;
+    for (var i = 0; i < aLength; i++) {
+      arrA.push(collection_a[i].key);
+    }
+    return arrA;
+  }
+  return result;
 }
 
 module.exports = collect_same_elements;
