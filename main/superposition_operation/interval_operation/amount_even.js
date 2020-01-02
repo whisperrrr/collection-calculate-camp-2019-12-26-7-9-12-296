c8723790879e1b600ca37579c9f52737382bbf36
JinHui <jh731448520@163.com>
2020-01-02 16:25:25 +0800

    [JinHui]{feat}-{#superposition}:finish 2.1.amount_even and pass test

--- main/superposition_operation/interval_operation/amount_even.js
+++ main/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function amount_even(collection) {
-
-  //在这里写入代码
+  var result = 0;
+  var arrLength = collection.length;
+  for (var i = 0; i < arrLength; i++) {
+    if( collection[i] % 2 === 0) {
+      result += collection[i];
+    }
+  }
+  return result;
 }
 
 module.exports = amount_even;
