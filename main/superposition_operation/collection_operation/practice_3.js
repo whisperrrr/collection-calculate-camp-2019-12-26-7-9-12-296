91ebe5594a1984a6f79da7bd4d8da899e75c57cd
JinHui <jh731448520@163.com>
2020-01-02 16:19:43 +0800

    [JinHui]{feat}-{#superposition}:finish 1.5.practice_3 and pass test

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  var oddArr = collection.filter(ele => ele % 2 !== 0);
+  var arrResult = oddArr.map(x => x * 3 + 5);
+  var result = arrResult.reduce((acc,cur) => acc + cur);
+  return result;
 }
 
 module.exports = hybrid_operation_to_uneven;
