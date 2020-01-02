d40bf60e6ddab438c2bb806b0774ddd018caf175
JinHui <jh731448520@163.com>
2020-01-02 16:03:07 +0800

    [JinHui]{feat}-{#superposition}:finish 1.1.letter_mapping_1 and pass test

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function even_to_letter(collection) {
-
-  //在这里写入代码
+  var str=['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var result = collection.filter(ele => ele % 2 === 0);
+  result = result.map(ele => str[ele]);
+  return result;
 }
 
 module.exports = even_to_letter;
