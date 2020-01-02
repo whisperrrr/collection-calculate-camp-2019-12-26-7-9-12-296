ed611145ceb19e24edbd98329492e712ae34da0f
JinHui <jh731448520@163.com>
2020-01-02 16:48:33 +0800

    [JinHui]{feat}-{#superposition}:finish 2.3.median_to_letter and pass test

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function median_to_letter(collection) {
-
-  //在这里写入代码
+  var str=[
+    '0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+    'aa','ab','ac','ad','ae','af','ag', 'ah', 'ai', 'aj', 'ak', 'al', 'am', 'an', 'ao', 'ap','aq', 'ar', 'as', 'at', 'au', 'av', 'aw', 'ax', 'ay','az', 
+    'ba'
+    ];
+  var arrMedian = compute_median(collection);
+  return str[Math.ceil(arrMedian)];
+}
+function compute_median(collection) {
+  var arrLength = collection.length;
+  collection = collection.sort((a,b) => a-b);
+  if (arrLength % 2 === 0) {
+    return (collection[arrLength/2] + collection[arrLength/2-1]) / 2;
+  }
+  else {
+    return collection[(arrLength-1)/2];
+  }
 }
 
 module.exports = median_to_letter;
