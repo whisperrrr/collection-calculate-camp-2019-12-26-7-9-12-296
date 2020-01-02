0891720ded5f042e9aa251514e85fa907a68da5e
JinHui <jh731448520@163.com>
2020-01-02 22:11:32 +0800

    [JinHui]{feat}-{#superposition}:finish 3.1.1 calculate_average and pass test

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,13 @@
 'use strict';
 var calculate_average = function(collection){
-
+  var result = 0;
+  var evenCount = 0;
+  var arrLength = collection.length;
+  for (var i = 2; i < arrLength;) {
+    result += collection[i];
+    i += 2;
+    evenCount += 1;
+  }
+  return result / evenCount;
 };
 module.exports = calculate_average;
