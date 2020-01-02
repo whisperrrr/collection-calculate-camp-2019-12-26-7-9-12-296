dfc4efac2a25b1c18cb46893e70e917cdc4c5196
JinHui <jh731448520@163.com>
2020-01-02 16:28:48 +0800

    [JinHui]{feat}-{#superposition}:finish 2.2.average_uneven and pass test

--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function average_uneven(collection) {
-
-  //在这里写入代码
+  var result = 0;
+  var arrLength = collection.length;
+  for (var i = 0; i < arrLength; i++) {
+    if( collection[i] % 2 !== 0) {
+      result += collection[i];
+    }
+  }
+  return result / (arrLength / 2);
 }
 
 module.exports = average_uneven;
