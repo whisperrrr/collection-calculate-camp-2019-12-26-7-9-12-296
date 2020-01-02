ea4c3df22ce58658acf1b9cf002152b9be89854b
JinHui <jh731448520@163.com>
2020-01-02 16:07:44 +0800

    [JinHui]{feat}-{#superposition}:finish 1.2.letter_mapping_2 and pass test

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function average_to_letter(collection) {
-
-  //在这里写入代码
+  var str=['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var arrSum = collection.reduce((acc,cur) => acc+cur);
+  var arrLength = collection.length;
+  var arrAverage = Math.ceil(arrSum / arrLength);
+  return str[arrAverage];
 }
 
 module.exports = average_to_letter;
