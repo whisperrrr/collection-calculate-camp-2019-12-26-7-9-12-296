4e80b29824319531fd598be8decde3ce9bb568c5
JinHui <jh731448520@163.com>
2020-01-03 01:58:24 +0800

    [JinHui]{feat}-{#superposition}:finish 3.1.5.single_element and pass test

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,25 @@
 'use strict';
-var single_element = function(collection){
-
+var single_element = function(collection){  
+  var evenArr = getEvenArr(collection);
+  var result = selectUnRepeat(evenArr);
+  return result;
 };
+function getEvenArr(collection) {
+  var evenArr = [];
+  var arrLength = collection.length;
+  for (var i = 1; i < arrLength;) {
+    evenArr.push(collection[i]);
+    i += 2;
+  }
+  return evenArr;
+}
+function selectUnRepeat(arr) {
+  var result = [];
+  arr.forEach(function(ele, i ,arr) {   
+  if(arr.indexOf(ele) === arr.lastIndexOf(ele)){
+   result.push(ele);
+  }
+ })
+ return result;
+}
 module.exports = single_element;
