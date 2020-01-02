ea38b0c747a0a21143511ea9db82091e46580bc3
JinHui <jh731448520@163.com>
2020-01-02 23:12:44 +0800

    [JinHui]{feat}-{#superposition}:finish 3.1.2 calculate_median and pass test

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,27 @@
 'use strict';
 var calculate_median = function(collection){
-
+  var evenArr = getEvenArr(collection);
+  var evenArrLength = evenArr.length;
+  var result = compute_median(evenArr);
+  return result;
 };
+function getEvenArr(collection) {
+  var evenArr = [];
+  var arrLength = collection.length;
+  for (var i = 1; i < arrLength;) {
+    evenArr.push(collection[i]);
+    i += 2;
+  }
+  return evenArr;
+}
+function compute_median(collection) {
+  var arrLength = collection.length;
+  collection = collection.sort((a,b) => a-b);
+  if (arrLength % 2 === 0) {
+    return (collection[arrLength/2] + collection[arrLength/2-1]) / 2;
+  }
+  else {
+    return collection[(arrLength-1)/2];
+  }
+}
 module.exports = calculate_median;
