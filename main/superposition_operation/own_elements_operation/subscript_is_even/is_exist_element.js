900c07cb13819984429defc6254f0a913ae48b24
JinHui <jh731448520@163.com>
2020-01-03 01:20:56 +0800

    [JinHui]{feat}-{#superposition}:finish 3.1.4.is_exist_element and pass test

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,19 @@
 'use strict';
 var is_exist_element = function(collection,element){
-
+  var evenArr = getEvenArr(collection);
+  var result = isInArr(evenArr,element);
+  return result;
 };
+function getEvenArr(collection) {
+  var evenArr = [];
+  var arrLength = collection.length;
+  for (var i = 2; i < arrLength;) {
+    evenArr.push(collection[i]);
+    i += 2;
+  }
+  return evenArr;
+}
+function isInArr(evenArr,element) {
+  return evenArr.indexOf(element) > -1;
+}
 module.exports = is_exist_element;
