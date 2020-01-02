58a4b39d150cbc072d7d040df490a458abceca71
JinHui <jh731448520@163.com>
2020-01-03 02:50:44 +0800

    [JinHui]{feat}-{#superposition}:finish 3.4.rank_by_two_largr_one_small and pass test

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,15 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+  var arrSorted = collection.sort((a,b) => a - b);
+  var result = [];
+  var arrLength = collection.length;
+  while (collection.length >= 3) {
+    var arrPiece = collection.splice(0,3);
+    var arrPieceMin = arrPiece.shift();
+    arrPiece.push(arrPieceMin);
+    result.push(arrPiece);
+  }
+  return result.flat().concat(collection);
+
 }
 module.exports = rank_by_two_large_one_small;
