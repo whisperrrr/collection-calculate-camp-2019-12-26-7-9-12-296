a1d29328c99b34baa203e9edb8c8a3d6ee304fd2
JinHui <jh731448520@163.com>
2020-01-03 10:22:39 +0800

    [JinHui]{fix}-{#section_1}:change 2.practice_2 flatten function

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,8 +1,13 @@
 function collect_same_elements(collection_a, collection_b) {
   var result;
-  collection_b = collection_b.flat();
+//  collection_b = collection_b.flat();
+  collection_b = flatten(collection_b);
   result = collection_a.filter(ele => collection_b.indexOf(ele) > -1);
   return result;
 }
-
+function flatten(arr) {
+  return arr.reduce((flat, toFlatten) => {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
 module.exports = collect_same_elements;
