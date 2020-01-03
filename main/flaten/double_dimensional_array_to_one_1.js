1d581fc08d9d38d9f10cfe534402ec274284b173
JinHui <jh731448520@163.com>
2020-01-03 10:19:25 +0800

    [JinHui]{fix}-{#flatten}:change 1&2. flatten function

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function double_to_one(collection) {
-  var result = collection.flat();
+//  var result = collection.flat();
+  var result = flatten(collection); 
   return result;
 }
-
+function flatten(arr) {
+  return arr.reduce((flat, toFlatten) => {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
 module.exports = double_to_one;
--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function double_to_one(collection) {
-  var result = collection.flat();
+//  var result = collection.flat();
+  var result = flatten(collection);
   result = result.filter(isRepeat);
   function isRepeat(ele,index) {
     if (result.indexOf(ele)===index) {
@@ -11,5 +12,9 @@ function double_to_one(collection) {
   }
   return result;
 }
-
+function flatten(arr) {
+  return arr.reduce((flat, toFlatten) => {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
 module.exports = double_to_one;
